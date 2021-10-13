2359dee724b923d5b634c1d86f313195530a4fab
toba-madamori <tobajamezmadamori@gmail.com>
2021-10-13 21:33:16 +0100

    added pagination for the home page and also redesigned the base home page

--- main/views.py
+++ main/views.py
@@ -1,13 +1,16 @@
+from django.core import paginator
 from django.shortcuts import render
 from django.views import View
 from .models import Product
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 # Create your views here.
 
-class IndexView(LoginRequiredMixin, View):
+class IndexView(View):
     def get(self, request, *args, **kwargs):
         products = Product.objects.all()
+
         context = {
             'products':products,
         }
@@ -15,11 +18,17 @@ class IndexView(LoginRequiredMixin, View):
         return render(request, 'main/index.html', context)
 
 
-class ShopView(LoginRequiredMixin, View):
+class ShopView(View):
     def get(self, request, *args, **kwargs):
         products = Product.objects.all()
+
+        paginator = Paginator(products, 6)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
         context = {
-            'products': products,
+            'page_obj': page_obj,
+            'paginator': paginator,
         }
 
         return render(request, 'main/shop.html', context)
